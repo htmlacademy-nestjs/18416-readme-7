6728e64380580959b97b9da441a03bf548941a42
Zotoff <paulzotoff@gmail.com>
2024-07-21 17:40:02 +0300

    Добавил класс для валидации через Class Validator. Пока что внедрять не буду, потому что говорят, глючное решение

---
+++ project/libs/account/config/src/configurations/mongodb/mongo.enum.ts
@@ -0,0 +1,5 @@
+export enum MongoConfigurationParams {
+  MIN_PORT = 0,
+  MAX_PORT = 65535,
+  DEFAULT_PORT = 27017,
+}
---
+++ project/libs/account/config/src/configurations/mongodb/mongo.messages.ts
@@ -0,0 +1,8 @@
+export enum MongoConfigurationMessages {
+  DB_HOST_REQUIRED = 'MongoDB Host is required',
+  DB_NAME_REQUIRED = 'MongoDB Name is required',
+  DB_PORT_REQUIRED = 'MongoDB Port is required',
+  DB_USER_REQUIRED = 'MongoDB User is required',
+  DB_PASSWORD_REQUIRED = 'MongoDB Password is required',
+  DB_BASE_AUTH_REQUIRED = 'MongoDB Base Authentication is required',
+}
