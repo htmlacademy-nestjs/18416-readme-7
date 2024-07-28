8950945e00635d9375751c52d36bf5b411c3ffab
Zotoff <paulzotoff@gmail.com>
2024-07-28 23:13:57 +0300

    Фикс интерфейсов для сущностей

--- project/libs/shared/core/src/lib/types/user.interface.ts
+++ project/libs/shared/core/src/lib/types/user.interface.ts
@@ -1,7 +1,8 @@
-import { UserRole } from './user-role.enum';
+import { Entity } from '../base/entity';
+import { UserRole } from './enums/user-role.enum';
 
-export interface User {
-  id?: string;
+export interface User extends Entity {
+  id: string;
   email: string;
   userName: string;
   registerDate: Date;
