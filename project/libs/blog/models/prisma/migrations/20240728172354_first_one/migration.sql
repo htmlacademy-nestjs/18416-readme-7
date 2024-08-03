5c68f52d0e0d2eedb8e10431f56abb419b778974
Zotoff <paulzotoff@gmail.com>
2024-08-03 12:50:57 +0300

    фикс

--- project/libs/blog/models/prisma/migrations/20240728172354_first_one/migration.sql
+++ project/libs/blog/models/prisma/migrations/20240728172354_first_one/migration.sql
@@ -41,14 +41,14 @@ CREATE TABLE "comments" (
 );
 
 -- CreateTable
-CREATE TABLE "Like" (
+CREATE TABLE "likes" (
     "id" TEXT NOT NULL,
     "user_id" TEXT NOT NULL,
     "post_id" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "Like_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "likes_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -58,4 +58,4 @@ CREATE INDEX "posts_name_idx" ON "posts"("name");
 ALTER TABLE "comments" ADD CONSTRAINT "comments_post_id_fkey" FOREIGN KEY ("post_id") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Like" ADD CONSTRAINT "Like_post_id_fkey" FOREIGN KEY ("post_id") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "likes" ADD CONSTRAINT "likes_post_id_fkey" FOREIGN KEY ("post_id") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
