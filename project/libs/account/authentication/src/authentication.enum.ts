64c8470d88429535fee87764b892e93829199e5a
Zotoff <paulzotoff@gmail.com>
2024-08-04 11:45:51 +0300

    Добавил валидации

--- project/libs/account/authentication/src/authentication.enum.ts
+++ project/libs/account/authentication/src/authentication.enum.ts
@@ -8,3 +8,8 @@ export enum AuthenticationResponseStatuses {
   RESPONSE_UNAUTHORZED = 'User is not authorized',
   RESPONSE_WRONG_PASSWORD = 'Wrong password',
 }
+
+export enum AuthenticationValidateMessages {
+  EMAIL_NOT_VALID = 'Email not valid',
+  DATE_REGISTER_NOT_VALID = 'Register date is not valid',
+}
