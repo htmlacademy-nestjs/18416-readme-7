22dc805ce389a5f270524c338eb7bdac23c84804
Zotoff <paulzotoff@gmail.com>
2024-08-06 14:21:20 +0300

    Добавил модуль поста и http для теста

---
+++ project/libs/module/blog/blog-post/src/post.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { PrismaClientModule } from '@project/blog-models';
+
+import { PostController } from './post.controller';
+import { PostService } from './post.service';
+import { PostRepository } from './post.repository';
+import { PostFactory } from './post.factory';
+
+@Module({
+  imports: [PrismaClientModule],
+  controllers: [PostController],
+  providers: [PostService, PostRepository, PostFactory],
+  exports: [PostService],
+})
+export class BlogPostModule {}
