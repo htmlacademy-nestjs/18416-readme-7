29d3e31ff0778c528e53a2d331e5e62e1eedf2e5
Zotoff <paulzotoff@gmail.com>
2024-08-06 15:07:30 +0300

    Подключение JWT модуля

--- project/libs/account/authentication/src/authentication.module.ts
+++ project/libs/account/authentication/src/authentication.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
 import { BlogUserModule } from '@project/blog-user';
+import { ConfigService } from '@nestjs/config';
+import { getJwtOptions } from '@project/account-config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [BlogUserModule],
+  imports: [
+    BlogUserModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: getJwtOptions,
+    }),
+  ],
   controllers: [AuthenticationController],
-  providers: [AuthenticationService],
+  providers: [AuthenticationService, JwtAccessStrategy],
 })
 export class AuthenticationModule {}
