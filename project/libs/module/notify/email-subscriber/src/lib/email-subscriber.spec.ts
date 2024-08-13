78e5f618e57290da3f424bda4994225107bec884
Zotoff <paulzotoff@gmail.com>
2024-08-13 19:59:52 +0300

    Работы по EmailSubscriber

--- project/libs/module/notify/email-subscriber/src/lib/email-subscriber.spec.ts
+++
@@ -1,7 +0,0 @@
-import { emailSubscriber } from './email-subscriber';
-
-describe('emailSubscriber', () => {
-  it('should work', () => {
-    expect(emailSubscriber()).toEqual('email-subscriber');
-  });
-});
--- project/libs/module/notify/email-subscriber/src/lib/email-subscriber.ts
+++
@@ -1,3 +0,0 @@
-export function emailSubscriber(): string {
-  return 'email-subscriber';
-}
