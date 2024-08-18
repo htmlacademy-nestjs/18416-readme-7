42d748abed3bd501380aa7b755aeaba3d7334c49
Zotoff <paulzotoff@gmail.com>
2024-08-18 22:27:38 +0300

    Работа с интерсепторами

--- project/apps/api/src/main.ts
+++ project/apps/api/src/main.ts
@@ -5,6 +5,7 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { RequestIdInterceptor } from '@project/interceptors';
 
 import { AppModule } from './app/app.module';
 
@@ -12,6 +13,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  app.useGlobalInterceptors(new RequestIdInterceptor());
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
--- project/libs/account/authentication/src/index.ts
+++ project/libs/account/authentication/src/index.ts
@@ -1,1 +1,3 @@
 export * from './authentication.module';
+export { LoginUserDto } from './dto/login-user.dto';
+export { CreateUserDto } from './dto/create-user.dto';
